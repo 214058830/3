d6679fd325faebca9e91475be7c65de3d9871f10
gongbin <214058830@qq.com>
2020-01-04 16:13:00 +0800

    2020-01-04

--- study/vue-basic-cdn/app.js
+++ study/vue-basic-cdn/app.js
@@ -4,7 +4,20 @@ new Vue({
     data() {
         return {
             name: 'gongbin',
-            qq: '214058830'
+            qq: '214058830',
+            website: 'https://www.baidu.com',
+            websiteTag: '<a href="http://www.taobao.com">taobao</a>'
         };
+    },
+    methods: { // 方法的定义
+        // 底下两种写法是一样的效果 推荐第二种
+        // greet: function () {
+        //     return 'Hello ' + this.name;
+        // }
+        greet(temp) {
+            // 底下两种写法效果一样 推荐第二种
+            // return 'Hello ' + this.name;
+            return `Hello ${this.name} ${temp}`;
+        }
     }
 });
