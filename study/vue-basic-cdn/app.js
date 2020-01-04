5ca4a58e291f1391e06f44877c2ccdad8a8f50de
gongbin <214058830@qq.com>
2020-01-04 15:13:03 +0800

    2020-01-04

---
+++ study/vue-basic-cdn/app.js
@@ -0,0 +1,10 @@
+// 实例化vue对象
+new Vue({
+    el: '#vue-app', // elemnet 表示要操作哪一个容器 -> div 只有vue-app的div块可以使用
+    data() {
+        return {
+            name: 'gongbin',
+            qq: '214058830'
+        };
+    }
+});
