87f4d0420b5facf0e463844f80220e1505a990fc
gongbin <214058830@qq.com>
2020-01-05 14:47:22 +0800

    2020-01-05

---
+++ study/vue-basic-cdn/v7/app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#vue-app',
+    data() {
+        return {
+            arr: ["abc", "def", "ghi"],
+            users: [
+                { name: "a", age: 3 },
+                { name: "b", age: 4 },
+                { name: "c", age: 5 }
+            ]
+        };
+    },
+    methods: {},
+    computed: {}
+});
