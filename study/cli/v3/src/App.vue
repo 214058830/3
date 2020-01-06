cf1b2a4113fc35eb6768a25893912948812cce7e
gongbin <214058830@qq.com>
2020-01-06 18:52:00 +0800

    2020-01-06

---
+++ study/cli/v3/src/App.vue
@@ -0,0 +1,25 @@
+<!-- 跟组件 -->
+<template>
+  <div id="app">
+    <HelloWorld/>
+    <h1>App.vue</h1>
+  </div>
+</template>
+
+<script>
+// 局部组件 import + components
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
