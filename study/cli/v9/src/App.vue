fa9168ea0691ba69705d43bafe1bb35d453fb4ba
gongbin <214058830@qq.com>
2020-01-07 16:46:19 +0800

    2020-01-07

---
+++ study/cli/v9/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- 缓存组件中的内容 -->
+    <keep-alive>
+      <!-- 用属性component来控制显示哪一个组件 -->
+      <component :is="component"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+// import form-one from ***;
+// import form-two from ***;
+
+export default {
+  name: "app",
+  data() {
+    return {
+      component: "form-one"
+    };
+  },
+  components: {
+    // form-one,
+    // form-two
+  },
+  methods: {}
+};
+</script>
+
+<style>
+</style>
