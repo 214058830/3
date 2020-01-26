0cbe8998d063d354816c8b19663d2f7e46352ada
gongbin <214058830@qq.com>
2020-01-26 18:44:38 +0800

    2020-01-26

--- service-ui/src/views/User.vue
+++ service-ui/src/views/User.vue
@@ -2,31 +2,25 @@
   <div class="layout-content">
     <Row>
       <Col span="6">
-        <Menu v-bind:active-name="select">
-          <Menu-group title="信息查询">
+        <Menu :active-name="MenuItem" @on-select="selectMenuItem">
+          <Menu-group title="用户信息管理">
             <Menu-item name="s1-1">
-              <Icon type="document-text"></Icon>水利信息查询
+              <Icon type="document-text"></Icon>录入信息
             </Menu-item>
           </Menu-group>
-          <Menu-group title="信息更改">
+          <Menu-group title="用户权限管理">
             <Menu-item name="s2-1">
-              <Icon type="heart"></Icon>录入信息
-            </Menu-item>
-            <Menu-item name="s2-2">
-              <Icon type="heart-broken"></Icon>信息修改
+              <Icon type="heart"></Icon>权限管理
             </Menu-item>
           </Menu-group>
         </Menu>
       </Col>
       <Col span="18">
-        <div v-show="select == 's1-1'">
+        <div v-show="MenuItem == 's1-1'">
           <p>s1-1</p>
         </div>
-        <div v-show="select == 's2-1'">
+        <div v-show="MenuItem == 's2-1'">
           <p>s2-1</p>
-        </div>
-        <div v-show="select == 's2-2'">
-          <p>s2-2</p>
         </div>
       </Col>
     </Row>
@@ -38,10 +32,14 @@ export default {
   components: {},
   data() {
     return {
-      select: "s1-1"
+      MenuItem: "s1-1"
     };
   },
-  methods: {},
+  methods: {
+    selectMenuItem(name) {
+      this.MenuItem = name;
+    }
+  },
   mounted() {}
 };
 </script>
