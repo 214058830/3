221c18d559763ddaeffbacf3fc3465752678a02b
gongbin <214058830@qq.com>
2020-02-05 15:50:14 +0800

    2020-02-05

--- service-ui/src/views/Water/AddInfo/AddInfo.vue
+++ service-ui/src/views/Water/AddInfo/AddInfo.vue
@@ -1,62 +1,105 @@
 <template>
-  <div style="text-align: center">
-    <Card v-if="this.current == 0">
-      <p style="margin-left: -750px" slot="title">录入水利信息</p>
-      <RadioGroup v-model="button" type="button">
-        <Row>
-          <Col>
-            <Radio id="radio" label="余额信息"></Radio>
-          </Col>
-          <Col>
-            <Radio id="radio" style="margin-top: 10px" label="缴费历史信息"></Radio>
-          </Col>
-        </Row>
-      </RadioGroup>
-    </Card>
-    <Card v-if="this.current == 1">
-      <p v-if="this.button == '余额信息'" style="margin-left: -750px" slot="title">输入信息</p>
-      <p v-if="this.button == '缴费历史信息'" style="margin-left: -750px" slot="title">上传缴费电子单</p>
-      <Amount v-if="this.button == '余额信息'" />
-      <ContributeHistory v-if="this.button == '缴费历史信息'" />
+  <div>
+    <Card>
+      <p slot="title">录入缴费历史信息</p>
+      <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="120">
+        <FormItem label="邮箱" prop="mail">
+          <Input v-model="formValidate.mail" style="width: 300px" placeholder="请输入Email"></Input>
+        </FormItem>
+        <FormItem label="缴费账单号" prop="history_id">
+          <Input v-model="formValidate.history_id" style="width: 300px" placeholder="请输入缴费账单号"></Input>
+        </FormItem>
+      </Form>
+      <Upload multiple type="drag" action="//jsonplaceholder.typicode.com/posts/">
+        <div style="padding: 20px 0">
+          <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+          <p>点击或拖拽文件到这里以上传</p>
+        </div>
+      </Upload>
     </Card>
     <div class="button">
-      <Button :disabled="lastdisable" type="primary" @click="last">上一步</Button>
-      <Button :disabled="nextdisable" style="margin-left: 10px" type="primary" @click="next">下一步</Button>
+      <Button :disabled="lastdisable" type="primary" @click="handleReset('formValidate')">清空</Button>
+      <Button
+        :disabled="nextdisable"
+        style="margin-left: 10px"
+        type="primary"
+        @click="submit('formValidate')"
+      >提交</Button>
     </div>
   </div>
 </template>
 
 <script>
-import Amount from "./Amount";
-import ContributeHistory from "./ContributeHistory";
 export default {
-  components: { ContributeHistory, Amount },
+  components: {},
   data() {
     return {
-      button: "余额信息",
-      current: 0
+      formValidate: {
+        mail: "",
+        history_id: ""
+      },
+      ruleValidate: {
+        mail: [
+          {
+            required: true,
+            message: "Mailbox cannot be empty",
+            trigger: "blur"
+          },
+          { type: "email", message: "Incorrect email format", trigger: "blur" }
+        ],
+        history_id: [
+          {
+            required: true,
+            message: "The account number cannot be empty",
+            trigger: "blur"
+          }
+        ]
+      }
     };
   },
   methods: {
-    next() {
-      this.current += 1;
+    handleReset(name) {
+      this.$refs[name].resetFields();
     },
-    last() {
-      this.current -= 1;
+    submit(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          // 登录的过渡动画
+          const msg = this.$Message.loading({
+            content: "Loading...",
+            duration: 0
+          });
+          this.axios
+            .post(
+              process.env.VUE_APP_BASE_URL +
+                process.env.VUE_APP_VERSION +
+                "/user/register",
+              this.formValidate
+            )
+            .then(
+              res => {
+                setTimeout(msg, 0);
+                let data = res.data;
+                if (data.code == 2000) {
+                  this.$Message.success("注册成功");
+                  this.$router.replace({ path: "/login" });
+                } else {
+                  this.$Message.warning("注册失败，" + data.msg);
+                }
+                console.log(res.data); // res 返回的是传出的参数
+              },
+              res => {
+                setTimeout(msg, 0);
+                this.$Message.warning("注册失败，请刷新或重试。");
+              }
+            );
+        } else {
+          this.$Message.warning("请输入正确的内容");
+        }
+      });
     }
   },
-  computed: {
-    nextdisable: {
-      get: function() {
-        return this.current >= 3;
-      }
-    },
-    lastdisable: {
-      get: function() {
-        return this.current <= 0;
-      }
-    }
-  },
+  computed: {},
   mounted() {}
 };
 </script>
@@ -70,7 +113,7 @@ export default {
   line-height: 60px;
 }
 .button {
-  float: right;
+  text-align: center;
   margin-top: 30px;
 }
 </style>
