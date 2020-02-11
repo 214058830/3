afc386cc4e2f928a1d5fb6b41d01f1350dd5177f
gongbin <214058830@qq.com>
2020-02-11 18:57:32 +0800

    2020-02-11

--- service-ui/src/components/Header.vue
+++ service-ui/src/components/Header.vue
@@ -7,40 +7,46 @@
       :active-name="activemenu"
       style="height: 90px; line-height:90px"
     >
-      <div class="layout-menu">
-        <img src="../assets/logo.png" style="transform: translateY(+25%); margin-left: -320px" />
-        <Menu-item style="margin-left: 80px" name="home" to="/">首页</Menu-item>
-        <Menu-item name="water" to="/water">水利信息系统</Menu-item>
-        <Menu-item name="user" to="/user" v-if="this.logo == 'true'">用户管理</Menu-item>
-        <Menu-item name="platform" to="/platform">论坛</Menu-item>
-      </div>
-      <div class="layout-registered" v-if="this.flag === 'true'">
-        <Dropdown
-          trigger="custom"
-          :visible="visible"
-          style="margin-right: 20px; cursor:pointer"
-          placement="bottom-end"
-        >
-          <div class="layout-center" @click="handleOpenAndClose">
-            你好， {{ this.mail }}
-            <Icon type="ios-arrow-up" v-if="this.visible" />
-            <Icon type="ios-arrow-down" v-else />
+      <Row>
+        <Col span="1">
+          <img src="../assets/logo.png" style="transform: translateY(+35%); margin-left: 150px" />
+        </Col>
+        <Col span="23">
+          <div class="layout-menu">
+            <Menu-item name="home" to="/">首页</Menu-item>
+            <Menu-item name="water" to="/water">水利信息系统</Menu-item>
+            <Menu-item name="user" to="/user" v-if="this.logo == 'true'">用户管理</Menu-item>
+            <Menu-item name="platform" to="/platform">论坛</Menu-item>
           </div>
-          <Dropdown-menu slot="list" style="width: 240px; margin-left: 30px">
-            <div style="height: 50px; margin-top: -10px" @click="center">
-              <Avatar icon="ios-person" size="small" />&thinsp; 个人中心
-            </div>
-            <div style="height: 60px; font-size: 16px" @click="logout">
-              退出
-              <Icon size="20" type="ios-power" style="margin-left: 150px" />
-            </div>
-          </Dropdown-menu>
-        </Dropdown>
-      </div>
-      <div class="layout-registered" v-else>
-        <Menu-item name="register" to="/register">注册</Menu-item>
-        <Menu-item name="login" to="/login">登录</Menu-item>
-      </div>
+          <div class="layout-registered" v-if="this.flag === 'true'">
+            <Dropdown
+              trigger="custom"
+              :visible="visible"
+              style="margin-right: 20px; cursor:pointer"
+              placement="bottom-end"
+            >
+              <div class="layout-center" @click="handleOpenAndClose">
+                你好， {{ this.mail }}
+                <Icon type="ios-arrow-up" v-if="this.visible" />
+                <Icon type="ios-arrow-down" v-else />
+              </div>
+              <Dropdown-menu slot="list" style="width: 240px; margin-left: 30px">
+                <div style="height: 50px; margin-top: -10px" @click="center">
+                  <Avatar icon="ios-person" size="small" />&thinsp; 个人中心
+                </div>
+                <div style="height: 60px; font-size: 16px" @click="logout">
+                  退出
+                  <Icon size="20" type="ios-power" style="margin-left: 150px" />
+                </div>
+              </Dropdown-menu>
+            </Dropdown>
+          </div>
+          <div class="layout-registered" v-else>
+            <Menu-item name="register" to="/register">注册</Menu-item>
+            <Menu-item name="login" to="/login">登录</Menu-item>
+          </div>
+        </Col>
+      </Row>
     </Menu>
   </div>
 </template>
@@ -98,7 +104,7 @@ export default {
 }
 .layout-menu {
   float: left;
-  margin-left: 100px;
+  margin-left: 180px;
 }
 .layout-registered {
   float: right;
