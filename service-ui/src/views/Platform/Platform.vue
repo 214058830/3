ab64373368c21f20e2d197c6936679fcfd28a212
gongbin <214058830@qq.com>
2020-02-13 22:40:38 +0800

    2020-02-13

--- service-ui/src/views/Platform/Platform.vue
+++ service-ui/src/views/Platform/Platform.vue
@@ -1,54 +1,102 @@
 <template>
   <div>
-    <List>
-      <ListItem>
-        <ListItemMeta
-          avatar="https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar"
-          title="This is title"
-          description="This is description, this is description."
-        />
-      </ListItem>
-      <ListItem>
-        <ListItemMeta
-          avatar="https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar"
-          title="This is title"
-          description="This is description, this is description."
-        />
-      </ListItem>
-      <ListItem>
-        <ListItemMeta
-          avatar="https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar"
-          title="This is title"
-          description="This is description, this is description."
-        />
-        <template slot="action">
-          <li>
-            <a href>Edit</a>
-          </li>
-          <li>
-            <a href>More</a>
-          </li>
-        </template>
-      </ListItem>
-      <ListItem>
-        <ListItemMeta
-          avatar="https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar"
-          title="This is title"
-          description="This is description, this is description."
-        />
-        <template slot="action">
-          <li>
-            <a href>Edit</a>
-          </li>
-          <li>
-            <a href>More</a>
-          </li>
-        </template>
-      </ListItem>
-    </List>
+    <Card style="margin-top: 10px">
+      <List>
+        <ListItem>
+          <ListItemMeta
+            avatar="https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar"
+            title="This is title"
+            description="This is description, this is description."
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemMeta
+            avatar="https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar"
+            title="This is title"
+            description="This is description, this is description."
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemMeta
+            avatar="https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar"
+            title="This is title"
+            description="This is description, this is description."
+          />
+          <template slot="action">
+            <li>
+              <a href>Edit</a>
+            </li>
+            <li>
+              <a href>More</a>
+            </li>
+          </template>
+        </ListItem>
+        <ListItem>
+          <ListItemMeta
+            avatar="https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar"
+            title="This is title"
+            description="This is description, this is description."
+          />
+          <template slot="action">
+            <li>
+              <a href>Edit</a>
+            </li>
+            <li>
+              <a href>More</a>
+            </li>
+          </template>
+        </ListItem>
+      </List>
+    </Card>
+    <div style="text-align: center; ">
+      <Page
+        style="margin-top: 20px;"
+        :total="this.page.total"
+        :current="this.page.number"
+        :page-size="this.page.size"
+        show-sizer
+        show-elevator
+        show-total
+        @on-change="changePageNumber"
+        @on-page-size-change="changePageSize"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      page: {
+        number: 1,
+        size: 10,
+        total: 0
+      }
+    };
+  },
+  methods: {
+    changePageNumber(num) {
+      this.page.number = num;
+      // this.updataTable();
+    },
+    // updataTable() {
+    //   this.data = [];
+    //   if (this.user_amount.length >= this.page.number * this.page.size) {
+    //     this.data = this.user_amount.slice(
+    //       (this.page.number - 1) * this.page.size,
+    //       this.page.number * this.page.size
+    //     );
+    //   } else {
+    //     this.data = this.user_amount.slice(
+    //       (this.page.number - 1) * this.page.size
+    //     );
+    //   }
+    // },
+    changePageSize(size) {
+      this.page.size = size;
+      // this.updataTable();
+    }
+  }
+};
 </script>
