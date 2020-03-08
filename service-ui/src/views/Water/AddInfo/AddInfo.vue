cda28368e1970dfa18a9358d46dcf4de9b100b11
gongbin <214058830@qq.com>
2020-03-08 16:14:21 +0800

    2020-03-08

--- service-ui/src/views/Water/AddInfo/AddInfo.vue
+++ service-ui/src/views/Water/AddInfo/AddInfo.vue
@@ -1,31 +1,43 @@
 <template>
   <div>
     <Card>
-      <p slot="title">录入缴费信息</p>
-      <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="120">
-        <FormItem label="邮箱" prop="mail">
+      <p slot="title">录入单位信息</p>
+      <Form ref="formData" :model="formData" :rules="ruleValidate" :label-width="120">
+        <FormItem label="单位名称" prop="company_name">
+          <Input v-model="formData.company_name" style="width: 300px" placeholder="请输入单位名称"></Input>
+        </FormItem>
+        <FormItem label="所属级别" prop="level">
+          <Select v-model="formData.level" style="width:200px">
+            <Option v-for="item in levelList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
+        </FormItem>
+        <FormItem label="负责人" prop="principal">
+          <Input v-model="formData.principal" style="width: 300px" placeholder="请输入负责人姓名"></Input>
+        </FormItem>
+        <FormItem label="电话" prop="telephone_number">
+          <Input v-model="formData.telephone_number" style="width: 300px" placeholder="请输入单位联系方式"></Input>
+        </FormItem>
+        <FormItem label="传真" prop="fax_number">
+          <Input v-model="formData.fax_number" style="width: 300px" placeholder="请输入传真号"></Input>
+        </FormItem>
+        <FormItem label="邮编" prop="post_code">
+          <Input v-model="formData.post_code" style="width: 300px" placeholder="请输入邮编号"></Input>
+        </FormItem>
+        <FormItem label="地址" prop="address">
+          <Input v-model="formData.address" style="width: 300px" placeholder="请输入单位地址"></Input>
+        </FormItem>
+        <FormItem label="单位简介" prop="introduction">
           <Input
-            :disabled="this.maildisabledflag"
-            v-model="formValidate.mail"
-            style="width: 300px"
-            placeholder="请输入Email"
-          ></Input>
-        </FormItem>
-        <FormItem label="缴费账单号" prop="history_id">
-          <Input v-model="formValidate.history_id" style="width: 300px" placeholder="请输入缴费账单号"></Input>
-        </FormItem>
-        <FormItem label="缴费金额" prop="amount">
-          <InputNumber
-            :max="99999.99"
-            style="width: 300px"
-            :precision="2"
-            :step="1.00"
-            v-model="formValidate.amount"
-          ></InputNumber>
+            v-model="formData.introduction"
+            :maxlength="introductionInputLength"
+            type="textarea"
+            placeholder="请输入单位简介"
+            :autosize="{minRows: 3,maxRows: 5}"
+          />
         </FormItem>
       </Form>
       <Upload
-        v-if="this.formValidate.file.length == 0"
+        v-if="this.formData.file.length == 0"
         :before-upload="handleUpload"
         accept="image/gif, image/jpeg, image/png, image/jpg"
         type="drag"
@@ -34,7 +46,7 @@
       >
         <div style="padding: 20px 0">
           <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
-          <p>点击或拖拽文件到这里以上传</p>
+          <p>点击或拖拽文件到这里上传单位图片</p>
         </div>
       </Upload>
       <Row v-else style="padding: 20px 0; height: 118px; text-align: center; position:relative;">
@@ -45,8 +57,8 @@
       </Row>
     </Card>
     <div class="button">
-      <Button type="primary" @click="handleReset('formValidate')">清空</Button>
-      <Button style="margin-left: 10px" type="primary" @click="submit('formValidate')">提交</Button>
+      <Button type="primary" @click="handleReset('formData')">清空</Button>
+      <Button style="margin-left: 10px" type="primary" @click="submit('formData')">提交</Button>
     </div>
   </div>
 </template>
@@ -60,29 +72,65 @@ export default {
   components: {},
   data() {
     return {
+      introductionInputLength: 64,
+      levelList: [
+        {
+          value: "厅属",
+          label: "厅属"
+        },
+        {
+          value: "市区",
+          label: "市区"
+        }
+      ],
       uploadUrl:
         process.env.VUE_APP_BASE_URL +
         process.env.VUE_APP_VERSION +
-        "/water/contribute_history/upload",
-      formValidate: {
-        mail: this.mail,
-        history_id: "",
-        amount: 0.0,
+        "/water/upload",
+      formData: {
+        company_name: "",
+        principal: "",
+        telephone_number: "",
+        fax_number: "",
+        post_code: "",
+        address: "",
+        level: "",
+        introduction: "",
         file: []
       },
       ruleValidate: {
-        mail: [
+        company_name: [
           {
             required: true,
-            message: "Mailbox cannot be empty",
+            message: "单位名称不能为空",
             trigger: "blur"
-          },
-          { type: "email", message: "Incorrect email format", trigger: "blur" }
+          }
         ],
-        history_id: [
+        telephone_number: [
           {
             required: true,
-            message: "The account number cannot be empty",
+            message: "联系电话不能为空",
+            trigger: "blur"
+          }
+        ],
+        address: [
+          {
+            required: true,
+            message: "地址不能为空",
+            trigger: "blur"
+          }
+        ],
+        level: [
+          {
+            required: true,
+            message: "所属级别不能为空",
+            trigger: "blur"
+          }
+        ],
+        introduction: [
+          {
+            required: true,
+            message: "单位简介不能为空",
             trigger: "blur"
           }
         ]
@@ -91,16 +139,17 @@ export default {
   },
   methods: {
     handleUpload(file) {
-      this.formValidate.file.push(file);
+      this.formData.file.push(file);
       return false;
     },
     handleReset(name) {
+      this.formData.file = [];
       this.$refs[name].resetFields();
     },
     submit(name) {
       this.$refs[name].validate(valid => {
         if (valid) {
-          if (this.formValidate.file.length == 0) {
+          if (this.formData.file.length == 0) {
             this.$Message.warning("上传文件不能为空");
             return;
           }
@@ -115,33 +164,31 @@ export default {
             }
           };
           // 处理file上传文件
-          let formData = new FormData();
-          for (let i = 0; i < this.formValidate.file.length; i++) {
-            formData.append("file", this.formValidate.file[i]);
+          let req = new FormData();
+          for (let i = 0; i < this.formData.file.length; i++) {
+            req.append("file", this.formData.file[i]);
           }
-          // 处理formValidate数据
+          // 处理formData数据
           const map = new Map([
-            ["mail", this.formValidate.mail],
-            ["history_id", this.formValidate.history_id],
-            ["amount", this.formValidate.amount]
+            ["company_name", this.formData.company_name],
+            ["principal", this.formData.principal],
+            ["telephone_number", this.formData.telephone_number],
+            ["fax_number", this.formData.fax_number],
+            ["post_code", this.formData.post_code],
+            ["address", this.formData.address],
+            ["level", this.formData.level],
+            ["introduction", this.formData.introduction]
           ]);
           for (let [key, value] of map) {
-            formData.append(key, value);
+            req.append(key, value);
           }
-          this.axios.post(this.uploadUrl, formData, config).then(
+          this.axios.post(this.uploadUrl, req, config).then(
             res => {
               setTimeout(msg, 0);
               let data = res.data;
               if (data.code == 2000) {
-                const title = "提交结果";
-                const content = "<p>预计三个工作日出现结果</p>";
-                this.$Modal.success({
-                  title: title,
-                  content: content,
-                  onOk: () => {
-                    this.$router.replace({ path: "/" });
-                  }
-                });
+                this.$Message.warning("提交成功");
+                this.handleReset("formData");
               } else {
                 this.$Message.warning("提交失败，" + data.msg);
               }
@@ -157,11 +204,7 @@ export default {
       });
     }
   },
-  computed: {
-    maildisabledflag: function() {
-      return this.logo == "false";
-    }
-  },
+  computed: {},
   mounted() {}
 };
 </script>
